1b0d74f219b707e236888f355cd50ab8071ccf22
leggo_my_diego <diegocast04@gmail.com>
2023-05-19 18:46:18 -0500

    Added github and linkedin social links to footer. Added copyright

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,5 +1,25 @@
+<script>
+	import Socials from "./Socials.svelte";
+</script>
 <footer>
-    <div class="center">
+    <div>
         <a href="/"><h1>Diego Castaneda</h1></a>
     </div>
+    <Socials />
+    <h6> © 2023 Diego Castaneda. All Rights Reserved. </h6>
 </footer>
+
+<style>
+    footer {
+        background-color: var(--color-bg-1);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding-top: 2rem;
+        gap: 1.5rem;
+    }
+    
+    h6 {
+        font-weight: 200;
+    }
+</style>
---
+++ src/routes/Socials.svelte
@@ -0,0 +1,37 @@
+<script>
+    import GithubIcon from './github.svelte';
+    import LinkedinIcon from './linkedin.svelte';
+</script>
+
+<div class="socials">
+    <a 
+        href="https://github.com/Diego-Castaneda" 
+        title="See my Github profile"
+        target="_blank"
+    >
+        <GithubIcon />
+    </a>
+    <a 
+        href="https://www.linkedin.com/in/diego-castaneda-lego/"
+        title="See my on LinkedIn"
+        target="_blank"
+    >
+        <LinkedinIcon />
+    </a>
+</div>
+
+<style>
+    .socials {
+        display: flex;
+        gap: 20px;
+        fill: var(--color-text);
+    }
+
+    .socials a {
+        width: 32px;
+    }
+
+    .socials a:hover {
+        filter: drop-shadow(0px 0px 3px var(--color-primary));
+    }
+</style>
