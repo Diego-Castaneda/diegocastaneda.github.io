05edc39e59b865a61882e26ac4b0cbe25a54792b
leggo_my_diego <diegocast04@gmail.com>
2023-05-31 11:58:31 -0500

    Moved navigation content to isolated component, NavBar.svelte

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,36 +1,19 @@
 <script>
 	import { page } from '$app/stores';
 	import ThemeToggle from './ThemeToggle.svelte';
-	import Menu from '$lib/icons/menu-outline.svelte';
 	import Wave from '$lib/components/wave.svelte';
-
+	import NavBar from '$lib/components/NavBar.svelte';
 </script>
 
 <header>
 	<div class="top-height"></div>
+
 	<div class="container flex-row">
-		<div class="logo">
-			<a href="/"><h1>Diego Castaneda</h1></a>
-		</div>
+		<div class="logo"><h1><a href="/">Diego Castaneda</a></h1></div>
 
-		<nav class="mobile-only-nav">
-			<Menu></Menu>
-		</nav>
+		<NavBar></NavBar>
+	</div>
 
-		<nav class="desktop-nav">
-			<ul class="page-links">
-				<li><a href="#">Latest</a></li>
-				<li><a href="#">Notes</a></li>
-				<li aria-current={$page.url.pathname === '/home' ? 'page' : undefined}>
-					<a href="/home">Homie</a>
-				</li>
-			</ul>
-
-			<div class="links">
-				<ThemeToggle />
-			</div>
-		</nav>
-	</div>	
 	<div class="bottom-height"></div>
 	<Wave />
 </header>
@@ -46,7 +29,6 @@
 		width: 100%;
 		height: 30px;
 		visibility: hidden;
-		position: absolute;
 	}
 
 	.bottom-height {
@@ -54,96 +36,15 @@
 		height: 90px;
 	}
 
-	.flex-row {
-		display: flex;
-		justify-content: flex-start;
-		align-items: center;
-	}
-
 	.logo {
 		display: flex;
 		justify-content: start;
-		min-width: 320px;
-		padding: 0;
-		margin-right: 2rem;
-	}
-
-	.mobile-only-nav {
-		width: 30px;
-		height: 30px;
-		margin-left: auto;
-	}
-
-	.desktop-nav {
-		display: none;
-	}
-
-	.links {
-		margin-left: auto;
-		display: flex;
-		align-items: center;
-	}
-
-	nav {
-		display: flex;
-		--background: rgba(255, 255, 255, 0.7);
-		width: 100%;
-	}
-
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: start;
-		align-items: center;
-		list-style: none;
-		background-size: contain;
-	}
-
-	li[aria-current='page']::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
-	}
-
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 0.5rem;
-		color: var(--color-text);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
-
-	a:hover {
-		color: var(--color-theme-1);
+		margin-right: 1rem;
 	}
 
 	@media (min-width: 615px) {
-		.mobile-only-nav {
-			display: none;
-		}
-		
-		.desktop-nav {
-			display: contents;
-		}	
-		
 		.top-height {
 			visibility: visible;
-			position: static;
 		}
 	}
 </style>
---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,104 @@
+<script>
+    import { page } from '$app/stores';
+    import ThemeToggle from './ThemeToggle.svelte';
+    import MenuIcon from '$lib/icons/menu-outline.svelte';
+</script>
+<div class="menu-icon mobile-only-nav">
+	<MenuIcon></MenuIcon>
+</div>
+
+<nav class="desktop-nav">
+	<ul class="page-links">
+		<li><a href="#">Latest</a></li>
+		<li><a href="#">Notes</a></li>
+		<li aria-current={$page.url.pathname === '/home' ? 'page' : undefined}>
+			<a href="/home">Homie</a>
+		</li>
+	</ul>
+
+	<div class="links">
+		<ThemeToggle />
+	</div>
+</nav>
+<style>
+    .menu-icon {
+        width: 50px;
+        height: 50px;
+        background: none;
+        border: none;
+        fill: none;
+        margin-left: auto;
+    }
+    
+    .mobile-only-nav {
+		margin-left: auto;
+	}
+
+	.desktop-nav {
+		display: none;
+	}
+
+	.links {
+		margin-left: auto;
+		display: flex;
+		align-items: center;
+	}
+
+	nav {
+		display: flex;
+		--background: rgba(255, 255, 255, 0.7);
+		width: 100%;
+	}
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 3em;
+		display: flex;
+		justify-content: start;
+		align-items: center;
+		list-style: none;
+		background-size: contain;
+	}
+
+	li[aria-current='page']::before {
+		--size: 6px;
+		content: '';
+		width: 0;
+		height: 0;
+		position: absolute;
+		top: 0;
+		left: calc(50% - var(--size));
+		border: var(--size) solid transparent;
+		border-top: var(--size) solid var(--color-theme-1);
+	}
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 0.5rem;
+		color: var(--color-text);
+		font-weight: 700;
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+	}
+
+	a:hover {
+		color: var(--color-theme-1);
+	}
+
+	@media (min-width: 615px) {
+		.mobile-only-nav {
+			display: none;
+		}
+		
+		.desktop-nav {
+			display: contents;
+		}	
+	}
+</style>
